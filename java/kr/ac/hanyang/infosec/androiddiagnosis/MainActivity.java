f8159dc4018ecd2b5355438d6aa44afc7704633f
Kanghyo Lee <onyourlife@naver.com>
2016-05-26 13:12:29 +0900

    Update MainActivity.java

--- java/kr/ac/hanyang/infosec/androiddiagnosis/MainActivity.java
+++ java/kr/ac/hanyang/infosec/androiddiagnosis/MainActivity.java
@@ -1,44 +1,136 @@
 package kr.ac.hanyang.infosec.androiddiagnosis;
 
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.ActivityInfo;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import android.app.Activity;
 import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.content.pm.ServiceInfo;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+public class MainActivity extends Activity {
 
-public class MainActivity extends AppCompatActivity {
+    List<String> groupList;
+    List<String> childList;
+    Map<String, List<String>> packageCollection;
+    ExpandableListView expListView;
+
+    PackageManager pm;
+    DiagnosisModule dm;
+    List<ApplicationInfo> applications;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String service = null;
+        pm = getPackageManager();
+        applications = pm.getInstalledApplications(PackageManager.GET_META_DATA);
 
-        PackageManager pm = getPackageManager();
-        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        createGroupList();
+        createCollection();
 
-        DiagnosisModule dm = new DiagnosisModule();
-        for (ApplicationInfo appInfo : packages) {
-            dm.getCertificate(pm, appInfo);
-            dm.getPermission(pm, appInfo);
-            dm.getReceiver(pm, appInfo);
-            service = dm.getService(pm, appInfo);
-            if (service != null) {
-                Log.e("Service: ", service);
+        expListView = (ExpandableListView) findViewById(R.id.laptop_list);
+
+        final ExpandableListAdapter expListAdapter = new ExpandableListAdapter(
+                this, groupList, packageCollection);
+        expListView.setAdapter(expListAdapter);
+
+        //setGroupIndicatorToRight();
+        expListView.setOnChildClickListener(new OnChildClickListener() {
+
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                final String selected = (String) expListAdapter.getChild(
+                        groupPosition, childPosition);
+                Toast.makeText(getBaseContext(), selected, Toast.LENGTH_LONG)
+                        .show();
+
+                return true;
             }
-        }
-        Log.e("Rooting", String.valueOf(dm.checkRooting()));
+        });
     }
+
+    private void createGroupList() {
+        groupList = new ArrayList<String>();
+        for (ApplicationInfo appInfo : applications) {
+            groupList.add(appInfo.packageName);
+        }
+    }
+
+    private void createCollection() {
+        ArrayList<String> permissions = null;
+        ArrayList<String> services = null;
+        ArrayList<String> receivers = null;
+        ArrayList<String> certificates = null;
+
+        String[] appPermission;
+        String[] appService;
+        String[] appReceiver;
+        String[] appCertificate;
+
+        packageCollection = new LinkedHashMap<String, List<String>>();
+
+        dm = new DiagnosisModule();
+        for (ApplicationInfo appInfo : applications) {
+            childList = new ArrayList<String>();
+
+            // permission 추가
+            permissions = dm.getPermission(pm, appInfo);
+            for (String permission : permissions) {
+                if (permission != null) {
+                    appPermission = permission.split(",");
+                    if (appInfo.packageName.equals(appPermission[0])) {
+//                        Log.e("appPermission[1]", appPermission[1]);
+                        childList.add(appPermission[1]);
+                    }
+                }
+            }
+
+            // Service 추가
+            services = dm.getService(pm, appInfo);
+            for (String service : services) {
+                if (service != null) {
+                    appService = service.split(",");
+                    if (appInfo.packageName.equals(appService[0])) {
+//                    Log.e("appService[1]", appService[1]);
+                        childList.add(appService[1]);
+                    }
+                }
+            }
+
+            // Receiver 추가
+            receivers = dm.getReceiver(pm, appInfo);
+            for (String receiver : receivers) {
+                if (receiver != null) {
+                    appReceiver = receiver.split(",");
+                    if (appInfo.packageName.equals(appReceiver[0])) {
+//                    Log.e("appService[1]", appService[1]);
+                        childList.add(appReceiver[1]);
+                    }
+                }
+            }
+
+            packageCollection.put(appInfo.packageName, childList);
+        }
+    }
+
+    private void loadChild(String[] laptopModels) {
+        childList = new ArrayList<String>();
+        for (String model : laptopModels)
+            childList.add(model);
+    }
+
 }
